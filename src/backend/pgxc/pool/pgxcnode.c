65506121eb1a1b88b522056b52b5b43bf642f5cc
Pavan Deolasee <pavan.deolasee@gmail.com>
2014-09-09 18:38:35 +0530

    Ensure pooler information is reloaded in active sessions after pgxc_pool_reload() is called
    InitMultinodeExecutor() takes care of reloading the datanode and coordinator
    handles after cluster configuration changes and user calls pgxc_pool_reload().
    This was only being done at the start of a session or in the session which
    directly calls pgxc_pool_reload(). But this is not enough for other existing
    backend connections. We fix that by calling InitMultinodeExecutor() while
    handling the pooler invalidation interrupt message

--- src/backend/pgxc/pool/pgxcnode.c
+++ src/backend/pgxc/pool/pgxcnode.c
@@ -2940,6 +2940,9 @@ DoInvalidateRemoteHandles(void)
 		handle->inStart = handle->inEnd = handle->inCursor = 0;
 		handle->outEnd = 0;
 	}
+
+	InitMultinodeExecutor(true);
+
 	return result;
 }
 #endif
